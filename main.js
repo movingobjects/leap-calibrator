22c99d1676a4fd15e934ab2f340d2bd33a4a05ef
Scott Thiessen <things@sccottt.com>
2018-01-23 11:50:21 -0800

    Add scripts, update packages, etc

---
+++ main.js
@@ -0,0 +1,25 @@
+
+const { app, BrowserWindow } = require('electron');
+
+require('electron-reload')(__dirname + '/app/build/');
+
+let win; // (prevents garbage collection)
+
+app.on('ready', () => {
+
+  win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    frame: false,
+    fullscreenable: true
+  });
+
+  win.loadURL(`file://${__dirname}/app/build/index.html`);
+
+});
+
+app.on('window-all-closed', () => {
+
+  app.quit();
+
+});
---
+++ scripts/clear-packages.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Emptying packages folder..."
+rm -rf packages/*
+
+echo "Complete"
+echo
